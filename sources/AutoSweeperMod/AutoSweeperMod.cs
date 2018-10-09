441849197a7a1ea9b665743c012bea91e05ebde8
rainbowdesign <andypu@zoho.com>
2018-10-10 01:09:51 +0200

    Add files via upload

--- sources/AutoSweeperMod/AutoSweeperMod.cs
+++ sources/AutoSweeperMod/AutoSweeperMod.cs
@@ -8,40 +8,50 @@ using ModHelper;
 
 namespace AutoSweeperMod
 {
-		[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigureComplete", null)]
-		public static class AutoSweeperMod
-		{
-			public static float sweeperpower = 120f;
-			public static int sweeperrange = 14;
+	[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigureComplete", null)]
+	public static class AutoSweeperMod
+	{
 
-			public static void Postfix(GameObject go)
-			{
-			ModHelper.ModHelper.ExposeConfig("sweeperpower", sweeperpower, "AutoSweeperMod");
-			ModHelper.ModHelper.ExposeConfig("sweeperrange", sweeperrange, "AutoSweeperMod");
-				go.AddOrGet<SolidTransferArm>().pickupRange = sweeperrange;
-				go.AddOrGet<StationaryChoreRangeVisualizer>().range = sweeperrange;
-				//configfile._streamwriter.Write("Load autosweeper");
-				//Debug.Log("Autosweepermod");
-				//configfile.moddebuglog("Autosweeper Loaded");
-			}
+		public static float sweeperpower = 0f;
+		public static int sweeperrange = 14;
+		public static void Postfix(GameObject go)
+		{
+			go.AddOrGet<SolidTransferArm>().pickupRange = sweeperrange;
+			go.AddOrGet<StationaryChoreRangeVisualizer>().height = sweeperrange;
+			go.AddOrGet<StationaryChoreRangeVisualizer>().width = sweeperrange;
+			//configfile._streamwriter.Write("Load autosweeper");
+			//Debug.Log("Autosweepermod");
+			//configfile.moddebuglog("Autosweeper Loaded");
 		}
+	}
 
-		[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigurePreview", null)]
-		public static class AutoSweeperModb
+	[HarmonyPatch(typeof(Grid), "IsPhysicallyAccessible", null)]
+	internal class AutoSweeperThroughWallsMod_Grid_IsPhysicallyAccessible
+	{
+		private static bool Prefix(ref bool __result)
 		{
-			public static void Postfix(GameObject go)
-			{
-				go.AddOrGet<StationaryChoreRangeVisualizer>().range = AutoSweeperMod.sweeperrange;
-			}
+			__result = true;
+			return false;
 		}
-		[HarmonyPatch(typeof(SolidTransferArmConfig), "CreateBuildingDef", null)]
-		public static class AutoSweeperModc
+	}
+
+	[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigurePreview", null)]
+	public static class AutoSweeperModb
+	{
+		public static void Postfix(GameObject go)
 		{
-			public static void Postfix(BuildingDef __result)
-			{
-				__result.EnergyConsumptionWhenActive = AutoSweeperMod.sweeperpower;
-			}
-		}/*
+			go.AddOrGet<StationaryChoreRangeVisualizer>().height = AutoSweeperMod.sweeperrange;
+			go.AddOrGet<StationaryChoreRangeVisualizer>().width = AutoSweeperMod.sweeperrange;
+		}
+	}
+	[HarmonyPatch(typeof(SolidTransferArmConfig), "CreateBuildingDef", null)]
+	public static class AutoSweeperModc
+	{
+		public static void Postfix(BuildingDef __result)
+		{
+			__result.EnergyConsumptionWhenActive = AutoSweeperMod.sweeperpower;
+		}
+	}/*
 	[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigureUnderConstruction", null)]
 	public static class AutoSweeperModd
 	{
